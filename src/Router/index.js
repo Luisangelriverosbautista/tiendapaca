19fe9fd0e9c35b7dfb395bd31598d7e6aa9d32dd
NayeliMoraless <158497599+NayeliMoraless@users.noreply.github.com>
2025-06-02 00:54:23 -0600

    index.js

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,29 +1,29 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router'
+import Principal from '@/components/Pages/Principal.vue'
 
-
-
-import registro from "/src/components/Pages/registrarseV.vue";
-// import Registro from "../page/registro.vue";
-import Principal from "/src/components/Pages/Principal.vue";
 
 const routes = [
-    {
-        path: "/",
-       
-        component: Principal,
+  {
+    path: '/',
+    name: 'principal',
+    component: Principal
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('@/components/Pages/Login.vue')
+  },
+  {
+    path: '/registrarseV',
+    name: 'RegistrarseV',
+    component: () => import('@/components/Pages/RegistrarseV.vue')
+  },
 
-    },
-    {
-        path: "/registro",
-        name: "registro",
-        component: registro,
-    }
-   
-    
-]
+] 
 
-const router = createRouter({ 
-    history: createWebHashHistory(import.meta.env.BASE_URL),
-    routes,
-});
-export default router;
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
